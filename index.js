4ebe968c51e85d72d51e2bae126b62763eb052e3
RedHood <alanohcz94@gmail.com>
2022-06-08 23:44:26 +0800

    remove helmet

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const mongoSanitize = require('express-mongo-sanitize');
-const helmet = require('helmet');
 
 const campgroundRoute = require('./routes/campground')
 const reviewRoute = require('./routes/review');
@@ -44,52 +43,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine('ejs', mate);
 app.use(mongoSanitize());
-app.use(helmet({contentSecurityPolicy : false})); // remember to update or settle contentSecurityPolicy
-
-// These are for ContentSecurityPolicy (Mainly is to allow which defined URL are able to be access/used/shown on to the browser)
-const scriptSrcUrls = [
-    "https://stackpath.bootstrapcdn.com/",
-    "https://api.tiles.mapbox.com/",
-    "https://api.mapbox.com/",
-    "https://kit.fontawesome.com/",
-    "https://cdnjs.cloudflare.com/",
-    "https://cdn.jsdelivr.net",
-];
-const styleSrcUrls = [
-    "https://kit-free.fontawesome.com/",
-    "https://stackpath.bootstrapcdn.com/",
-    "https://api.mapbox.com/",
-    "https://api.tiles.mapbox.com/",
-    "https://fonts.googleapis.com/",
-    "https://use.fontawesome.com/",
-];
-const connectSrcUrls = [
-    "https://api.mapbox.com/",
-    "https://a.tiles.mapbox.com/",
-    "https://b.tiles.mapbox.com/",
-    "https://events.mapbox.com/",
-];
-const fontSrcUrls = [];
-app.use(
-    helmet.contentSecurityPolicy({
-        directives: {
-            defaultSrc: [],
-            connectSrc: ["'self'", ...connectSrcUrls],
-            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
-            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-            workerSrc: ["'self'", "blob:"],
-            objectSrc: [],
-            imgSrc: [
-                "'self'",
-                "blob:",
-                "data:",
-                "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
-                "https://images.unsplash.com/",
-            ],
-            fontSrc: ["'self'", ...fontSrcUrls],
-        },
-    })
-);
 
 //app.use applies it's expression to every request
 app.use(express.urlencoded({extended:true}));
@@ -145,6 +98,6 @@ app.use((err, req, res, next)=> {
 })
 
 
-app.listen(8000, ()=>{
-    console.log("APP started in port 8000")
+app.listen(3000, ()=>{
+    console.log("APP started in port 3000")
 })
